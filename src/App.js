7d2654aadff684e9bf9ccbfb5394f9eed05863b2
iamshm <krynx9.9@gmail.com>
2021-11-28 01:38:32 +0530

    Modal done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import Cart from "components/Cart/Cart";
 import Header from "components/Layout/Header";
 import Meals from "components/Meals/Meals";
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const App = () => {
+  const [cartIsShown, setCartIsShown] = useState(false);
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
   return (
     <Fragment>
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
---
+++ src/components/Cart/CartItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CartItem = () => {
+  return <div>jfla</div>;
+};
+
+export default CartItem;
