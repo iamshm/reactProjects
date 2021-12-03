70a30f53096dece66969c34d9023d8ae76fa5887
iamshm <krynx9.9@gmail.com>
2021-12-03 19:54:44 +0530

    done app

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,34 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartIcon from "components/Cart/CartIcon";
 import styles from "./HeaderCartButton.module.css";
 import CartContext from "store/cart-context";
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
-  const numberOfCartItems = cartCtx.items.reduce((currNumber, item) => {
+  const { items } = cartCtx;
+  const [btnHigh, setBtnHigh] = useState(false);
+  const numberOfCartItems = items.reduce((currNumber, item) => {
     return currNumber + item.amount;
   }, 0);
+  const btnStyles = `${styles.button} ${btnHigh ? styles.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnHigh(true);
+
+    const timer = setTimeout(() => {
+      setBtnHigh(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={btnStyles} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
