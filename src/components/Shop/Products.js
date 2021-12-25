de86b9fa3d392e50f1ef8cf2d9f7e77f51d95fbd
iamshm <krynx9.9@gmail.com>
2021-12-25 07:26:39 +0530

    basic reducers added

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,41 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    name: "My First Second",
+    description: "This is describing first",
+  },
+  {
+    id: "p2",
+    price: 19,
+    name: "My Second Bath",
+    description: "This is describing second",
+  },
+  {
+    id: "p3",
+    price: 16,
+    name: "My Soap Second",
+    description: "This is describing third",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.name}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
