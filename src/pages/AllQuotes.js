1dc9d2c4089dff218364df8232664eb4607ba53c
iamshm <krynx9.9@gmail.com>
2022-01-04 19:58:41 +0530

    Firebase joined

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,24 @@
+import { useContext } from "react";
+import AppContext from "store/context";
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  const { quotes } = useContext(AppContext);
+
+  return (
+    <div>
+      <h1>Quotes</h1>
+      <ul>
+        {quotes.map((q) => (
+          <li key={q.text}>
+            <div>
+              <h2>{q.text}</h2>
+              <h6>{q.author}</h6>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default AllQuotes;
--- src/store/context.js
+++ src/store/context.js
@@ -1,6 +1,7 @@
-const { createContext, useState } = require("react");
+import { createContext, useState, useEffect } from "react";
 
 const AppContext = createContext({
+  quotes: [],
   darkMode: true,
   darkModeToggle: () => {},
 });
@@ -9,6 +10,7 @@ export default AppContext;
 
 export const AppContextProvider = (props) => {
   const [darkMode, setDarkMode] = useState(true);
+  const [quotes, setQuotes] = useState([]);
 
   const darkModeToggle = () => {
     setDarkMode((prevDarkState) => {
@@ -16,9 +18,53 @@ export const AppContextProvider = (props) => {
     });
   };
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(
+        "https://react-http-bd594-default-rtdb.firebaseio.com/quoteDB.json"
+      );
+      const resData = await res.json();
+      return resData;
+    };
+    fetchData().then((data) => {
+      //fetch data Mode
+      if (data.darkState) {
+        setDarkMode(true);
+      } else {
+        setDarkMode(false);
+      }
+      //fetch quotes
+      const quoteList = [];
+      for (const i in data.quotes) {
+        quoteList.push(data.quotes[i]);
+      }
+      setQuotes(quoteList);
+    });
+  }, []);
+
+  useEffect(() => {
+    const sendData = async () => {
+      const res = await fetch(
+        "https://react-http-bd594-default-rtdb.firebaseio.com/quoteDB.json",
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            quotes: quotes,
+            darkState: darkMode ? 1 : 0,
+          }),
+        }
+      );
+      if (!res.ok) {
+        throw new Error("Error");
+      }
+    };
+    sendData().catch((err) => console.log(err));
+  }, [quotes, darkMode]);
+
   return (
     <AppContext.Provider
       value={{
+        quotes,
         darkMode: darkMode,
         darkModeToggle: darkModeToggle,
       }}
